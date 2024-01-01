8a84a4b357d67f0f282231a60e543160eae276b8
Nguyễn Thiện Hùng <nthung@ninepoints.vn>
2024-01-02 01:43:11 +0700

    - Fix bugs: not support if IN_LIST. - Upgrade DCMTK. - Remove ICU.

--- Modules/ThirdParty/DCMTK/itk-module-init.cmake
+++ Modules/ThirdParty/DCMTK/itk-module-init.cmake
@@ -27,27 +27,5 @@ if(ITK_USE_SYSTEM_DCMTK)
   find_package(DCMTK REQUIRED NO_MODULE)
 else(ITK_USE_SYSTEM_DCMTK)
   # Copied and mofified from DCMTK/CMake/3rdparty.cmake
-  if(NOT DEFINED DCMTK_USE_ICU)
-    include(CheckCXXSourceCompiles)
-    check_cxx_source_compiles("#include <iconv.h>\nint main(){iconv_t cd = iconv_open(\"\",\"\");iconv(cd,0,0,0,0);iconv_close(cd);return 0;}" WITH_STDLIBC_ICONV)
-    if(WITH_STDLIBC_ICONV)
-      message(STATUS "Info: found builtin ICONV support inside the C standard library.")
-      set(CHARSET_CONVERSION_ARGS
-        -DDCMTK_WITH_STDLIBC_ICONV:BOOL=ON
-        -DDCMTK_WITH_ICU:BOOL=OFF
-        "-DDCMTK_ENABLE_CHARSET_CONVERSION:STRING=stdlibc (iconv)"
-        CACHE INTERNAL "DCMTK Internal arguments"
-        )
-    endif()
-    # ICU creates problems on MacOS and Windows, so by default it is disabled.
-    # On Linux, the C standard library can have builtin ICONV support. We
-    # disable building ICU by default.
-    option(DCMTK_USE_ICU "Downloads and compile ICU for DCMTK" OFF)
-  endif()
-  if(DCMTK_USE_ICU)
-    if(ITK_USE_SYSTEM_ICU)
-      find_package(ICU REQUIRED COMPONENTS uc data)
-      set(ITKDCMTK_ICU_LIBRARIES ${ICU_LIBRARIES})
-    endif()
-  endif()
+  include(CheckCXXSourceCompiles)
 endif(ITK_USE_SYSTEM_DCMTK)
